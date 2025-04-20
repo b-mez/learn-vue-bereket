cf8bbd9c8eddf63f0d41714f507cb21fecde1004
b-mez <b.mezgebu@tcu.edu>
2025-04-20 18:23:08 -0500

    Completed Programmatic Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,19 @@
 <template>
-  <h1>message: {{ message }}</h1>
-  <hr />
-  <h1>number: {{ number }}</h1>
-  <hr />
-  <h1>doubleNum(50): {{ doubleNum(50) }}</h1>
-  <hr />
-  <h1>number * 2 = {{ number * 2 }}</h1>
-  <hr />
-  <h1>{{ number > 150 ? "number is greater than 150" : "number is less than 150" }}</h1>
-  <hr />
-  <h1 v-text="number"></h1>
-  <h1>{{ harry }}</h1>
-  <h1>{{ harry.name }}</h1>
-  <h1>{{ hogwartsWizards }}</h1>
-  <h1>{{ hogwartsWizards[0] }}</h1>
-  <hr />
-  <h1>rawHtml: {{ rawHtml }}</h1>
-
-  <h1 v-text="rawHtml"></h1>
-
-  <h1 v-html="rawHtml"></h1>
+  <div class="app">
+    <Navigation></Navigation>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script setup>
-let message = 'Hello, Vue'
-let number = 50
+import Navigation from '@/components/Navigation.vue';
 
-function doubleNum(num) {
-  return num * 2
-}
-
-let harry = {
-  id: 1001,
-  name: 'Harry Potter',
-  house: 'Gryffindor',
-  age: 17, // Age during the final battle of Hogwarts
-  wand: {
-    core: 'Phoenix feather',
-    wood: 'Holly'
-  }
-}
-const hogwartsWizards = [
-  {
-    id: 1001,
-    name: 'Harry Potter',
-    house: 'Gryffindor',
-    age: 17,
-    wand: {
-      core: 'Phoenix feather',
-      wood: 'Holly'
-    }
-  },
-  {
-    id: 1002,
-    name: 'Hermione Granger',
-    house: 'Gryffindor',
-    age: 17,
-    wand: {
-      core: 'Dragon heartstring',
-      wood: 'Vine'
-    }
-  },
-  {
-    id: 1003,
-    name: 'Ron Weasley',
-    house: 'Gryffindor',
-    age: 17,
-    wand: {
-      core: 'Unicorn hair',
-      wood: 'Willow'
-    }
-  },
-  {
-    id: 1004,
-    name: 'Draco Malfoy',
-    house: 'Slytherin',
-    age: 17,
-    wand: {
-      core: 'Dragon heartstring',
-      wood: 'Hawthorn'
-    }
-  }
-]
-let rawHtml = '<span style="color: red">This should be red.</span>'
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.app {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
---
+++ src/views/BlogPosts.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="blog-posts-layout">
+        <aside class="blog-post-list">
+            <BlogPostList></BlogPostList>
+        </aside>
+        <main class="blog-post-content-with-sidebar">
+            <!-- This is the default router-view -->
+            <router-view class="blog-post-content"></router-view>
+            <!-- This is the named router-view -->
+            <router-view name="sidebar"></router-view>
+        </main>
+    </div>
+</template>
+
+<script setup>
+import BlogPostList from '@/components/BlogPostList.vue'
+</script>
+
+<style lang="scss" scoped>
+.blog-posts-layout {
+    display: flex;
+    width: 100%;
+    gap: 1rem;
+
+    .blog-post-list {
+        flex: 0 0 25%
+    }
+
+    .blog-post-content-with-sidebar {
+        display: flex;
+        flex: 0 0 75%;
+
+        .blog-post-content {
+            flex: 1; // Take up the remaining space
+        }
+    }
+}
+</style>
